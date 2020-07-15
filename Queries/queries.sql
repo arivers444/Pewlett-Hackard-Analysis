3682c938c65c96f7ef30302799469aad96923813
angel <angelabarrett@msn.com>
2020-07-15 16:35:24 -0500

    Added queries & data files

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -182,3 +182,44 @@ ON (e.emp_no = de.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 AND (de.to_date = '9999-01-01');
+	
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);	
+		
+-- Department Retirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name	
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no);
+		
+-- Module 7.3.6
+-- Create a query that will return only the information relevant to the Sales team. 
+-- The requested list includes:
+-- Employee numbers
+-- Employee first name
+-- Employee last name
+-- Employee department name
+
+-- Create another query that will return the following information for the Sales and Development teams:
+-- Employee numbers
+-- Employee first name
+-- Employee last name
+-- Employee department name
