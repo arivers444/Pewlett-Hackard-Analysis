c31035e3f17bc1ea44fdc78194834e846cc6ec2f
angel <angelabarrett@msn.com>
2020-07-16 15:16:22 -0500

    Added Challenge Info

--- Queries/Module_Queries.sql
+++ Queries/Module_Queries.sql
@@ -115,16 +115,16 @@ SELECT departments.dept_name,
 	dept_manager.from_date,
 	dept_manager.to_date
 FROM departments
-INNER JOIN dept_manager
-ON departments.dept_no = dept_manager.dept_no;
+	INNER JOIN dept_manager
+		ON departments.dept_no = dept_manager.dept_no;
 -- Shorten above code
 SELECT d.dept_name,
 	dm.emp_no,
 	dm.from_date,
 	dm.to_date
 FROM departments as d
-INNER JOIN dept_manager as dm
-ON d.dept_no = dm.dept_no;
+	INNER JOIN dept_manager as dm
+		ON d.dept_no = dm.dept_no;
 
 -- Joining retirement_info and dept_emp tables
 SELECT retirement_info.emp_no,
@@ -132,8 +132,8 @@ SELECT retirement_info.emp_no,
 	retirement_info.last_name,
 	dept_emp.to_date
 FROM retirement_info
-LEFT JOIN dept_emp
-ON retirement_info.emp_no = dept_emp.emp_no;
+	LEFT JOIN dept_emp
+		ON retirement_info.emp_no = dept_emp.emp_no;
 
 DROP TABLE current_emp;
 -- Shorten above code
@@ -143,8 +143,8 @@ SELECT ri.emp_no,
 	de.to_date
 INTO current_emp
 FROM retirement_info as ri
-LEFT JOIN dept_emp as de
-ON ri.emp_no = de.emp_no
+	LEFT JOIN dept_emp as de
+		ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
 -- Check the table
 SELECT * FROM retirement_info;
@@ -155,8 +155,8 @@ SELECT COUNT(ce.emp_no),
 	de.dept_no
 INTO retire_by_dept
 FROM current_emp as ce
-LEFT JOIN dept_emp as de
-ON ce.emp_no = de.emp_no
+	LEFT JOIN dept_emp as de
+		ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 -- Check the table
@@ -175,10 +175,10 @@ SELECT e.emp_no,
 	de.to_date
 INTO emp_info
 FROM employees as e
-INNER JOIN salaries as s
-ON (e.emp_no = s.emp_no)
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
+	INNER JOIN salaries as s
+		ON (e.emp_no = s.emp_no)
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 AND (de.to_date = '9999-01-01');
@@ -248,6 +248,8 @@ FROM current_emp as ce
 WHERE d.dept_no IN ('d007', 'd005');
 SELECT * FROM sales_and_dev_team_info;
 
+
+
 -- Challenge
 -- Create table to find job title names.
 SELECT COUNT(e.emp_no),
@@ -258,12 +260,15 @@ FROM employees as e
 		ON e.emp_no = t.emp_no
 GROUP BY t.title;
 
+
 -- Create a table containing the number of employees about to retire grouped by job title.
+DROP TABLE retire_by_title;
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
 	t.title,
 	t.from_date,
+	t.to_date,
 	s.salary
 INTO retire_by_title 
 FROM employees as e
@@ -272,5 +277,23 @@ FROM employees as e
 	INNER JOIN salaries AS s
 		ON (e.emp_no = s.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND t.title IN ('Assistant Engineer', 'Engineer', 'Manager', 'Senior Engineer', 'Senior Staff', 'Staff', 'Technique Leader');
+AND t.title IN ('Assistant Engineer', 'Engineer', 'Manager', 'Senior Engineer', 'Senior Staff', 'Staff', 'Technique Leader')
+ORDER BY emp_no;
+-- Query the table.
+SELECT * FROM retire_by_title;
+
+-- Partition the data to show only most recent title per employee.
+DROP TABLE future_retirees_by_title;
+SELECT emp_no, first_name, last_name, title, from_date, salary
+-- INTO future_retirees_by_title
+FROM 
+(SELECT emp_no, first_name, last_name, title, from_date, to_date, salary, 
+ ROW_NUMBER() OVER
+ (PARTITION BY (emp_no)
+ ORDER BY to_date DESC) rn
+ FROM retire_by_title) tmp 
+ WHERE rn = 1
+ORDER BY emp_no;
+-- Query the table.
+SELECT * FROM future_retirees_by_title;
 
